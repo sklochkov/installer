9c6c14b7c86aa7bd7a0d57076c13f657f9033d83
klochkov <klochkov@bz-repo.(none)>
2013-07-24 17:53:24 +0400

    add initial discover script stubs

---
+++ bootstrapper.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BASE="http://192.168.10.169"
+STAGE2_URL="${BASE}/discover.py"
+TARGET=/usr/local/sbin/discover.py
+
+wget -qO "${TARGET}" "${STAGE2_URL}"
+
+chmod +x "${TARGET}"
+
+cat << EOF > /etc/cron.d/discover
+* * * * * $TARGET check
+EOF
+
+cat << EOF > /etc/discover.conf
+base = ${BASE}
+EOF
+
+exec "${TARGET}" discover
+
